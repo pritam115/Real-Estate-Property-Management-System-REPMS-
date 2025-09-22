332c1633f96eeee262f7cd31f3f0158ab4b0de80
Pritam Saha Turja <110053926+pritam115@users.noreply.github.com>
2025-09-23 05:12:25 +0600

    Add files via upload

---
+++ calc.php
@@ -0,0 +1,192 @@
+<?php 
+ini_set('session.cache_limiter','public');
+session_cache_limiter(false);
+session_start();
+include("config.php");
+
+// codeing
+
+if(isset($_REQUEST['calc']))
+{
+	$amount=$_REQUEST['amount'];
+	$mon=$_REQUEST['month'];
+	$int=$_REQUEST['interest'];
+	
+	$interest = $amount * $int/100;
+	$pay = $amount + $interest;
+	$month = $pay / $mon;
+
+}	
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Meta Tags -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="shortcut icon" href="images/favicon.ico">
+
+<!--	Fonts
+	========================================================-->
+<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+
+<!--	Css Link
+	========================================================-->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="css/layerslider.css">
+<link rel="stylesheet" type="text/css" href="css/color.css">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/login.css">
+ 
+<!--	Title
+	=========================================================-->
+<title>REPMS</title>
+</head>
+<body>
+
+<!--	Page Loader
+=============================================================
+<div class="page-loader position-fixed z-index-9999 w-100 bg-white vh-100">
+	<div class="d-flex justify-content-center y-middle position-relative">
+	  <div class="spinner-border" role="status">
+		<span class="sr-only">Loading...</span>
+	  </div>
+	</div>
+</div>
+--> 
+
+
+<div id="page-wrapper">
+    <div class="row"> 
+        <!--	Header start  -->
+		<?php include("include/header.php");?>
+        <!--	Header end  -->
+        
+        <!--	Banner   --->
+        <!-- <div class="banner-full-row page-banner" style="background-image:url('images/breadcromb.jpg');">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="page-name float-left text-white text-uppercase mt-1 mb-0"><b>User Listed Property</b></h2>
+                    </div>
+                    <div class="col-md-6">
+                        <nav aria-label="breadcrumb" class="float-left float-md-right">
+                            <ol class="breadcrumb bg-transparent m-0 p-0">
+                                <li class="breadcrumb-item text-white"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">User Listed Property</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div> --> 
+         <!--	Banner   --->
+		 
+		 
+		<!--	Submit property   -->
+        <div class="full-row bg-gray">
+            <div class="container">
+                    <div class="row mb-5">
+						<div class="col-lg-12">
+							
+                        </div>
+					</div>
+					<div class="full-row bg-gray">
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-lg-12">
+                <h2 class="text-secondary double-down-line text-center">EMI Calculator</h2>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
+                <form method="post" action="">
+                    <div class="form-group">
+                        <label for="amount">Loan Amount ($)</label>
+                        <input type="number" name="amount" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="month">Loan Duration (Months)</label>
+                        <input type="number" name="month" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="interest">Interest Rate (%)</label>
+                        <input type="number" name="interest" class="form-control" step="0.01" required>
+                    </div>
+                    <button type="submit" name="calc" class="btn btn-primary">Calculate EMI</button>
+                </form>
+            </div>
+        </div>
+
+        <?php if (isset($_POST['calc'])): ?>
+        <div class="row justify-content-center mt-5">
+            <div class="col-lg-6">
+                <table class="table table-bordered table-hover">
+                    <thead class="bg-secondary text-white">
+                        <tr>
+                            <th>Term</th>
+                            <th>Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>Amount</td><td>$<?php echo $amount; ?></td></tr>
+                        <tr><td>Total Duration</td><td><?php echo $mon; ?> Months</td></tr>
+                        <tr><td>Interest Rate</td><td><?php echo $int; ?>%</td></tr>
+                        <tr><td>Total Interest</td><td>$<?php echo number_format($interest, 2); ?></td></tr>
+                        <tr><td>Total Amount</td><td>$<?php echo number_format($pay, 2); ?></td></tr>
+                        <tr><td>Monthly EMI</td><td>$<?php echo number_format($month, 2); ?></td></tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <?php endif; ?>
+    </div>
+</div>
+
+            </div>
+        </div> 
+	<!--	Submit property   -->
+        
+        
+        <!--	Footer   start-->
+		<?php include("include/footer.php");?>
+		<!--	Footer   start-->
+        
+        <!-- Scroll to top --> 
+        <a href="#" class="bg-secondary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
+        <!-- End Scroll To top --> 
+    </div>
+</div>
+<!-- Wrapper End --> 
+
+<!--	Js Link
+============================================================--> 
+<script src="js/jquery.min.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/greensock.js"></script> 
+<script src="js/layerslider.transitions.js"></script> 
+<script src="js/layerslider.kreaturamedia.jquery.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/tmpl.js"></script> 
+<script src="js/jquery.dependClass-0.1.js"></script> 
+<script src="js/draggable-0.1.js"></script> 
+<script src="js/jquery.slider.js"></script> 
+<script src="js/wow.js"></script> 
+<script src="js/custom.js"></script>
+</body>
+</html>
